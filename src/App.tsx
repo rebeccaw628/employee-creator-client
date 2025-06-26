4e5472d28f0f541cd839cc9c82b59f63c7bea4bc
rebeccaw628 <rebecca.wu628@gmail.com>
2025-06-26 13:08:42 +1000

    sets up basic app structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router";
+import "./App.css";
+import DashboardPage from "./pages/DashboardPage";
+import EmployeesPage from "./pages/EmployeesPage";
+import EmployeePage from "./pages/EmployeePage";
 
 function App() {
-  
   return (
-    <>
-     
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<DashboardPage />} />
+        <Route path="/employees" element={<EmployeesPage />} />
+        <Route path="/employee/:id" element={<EmployeePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/NavBarItem/NavBarItem.tsx
@@ -0,0 +1,5 @@
+const NavBarItem = () => {
+  return <div>NavBarItem</div>;
+};
+
+export default NavBarItem;
